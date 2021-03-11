527ce6661c89eb592c14fa73816c77f732f60eb2
yenhsi <cynthiahsieh218@gmail.com>
2021-03-11 17:27:52 +0800

    homework customer

--- src/main/java/com/cc/Customer.java
+++ src/main/java/com/cc/Customer.java
@@ -4,15 +4,13 @@ public class Customer {
     String id;
     int cost;
     int discount;
-    int total;
-    public Customer(String id, int cost, int discount, int total){
+
+    public Customer(String id, int cost, int discount){
         this.id = id;
         this.cost = cost;
         this.discount = discount;
-        this.total = total;
     }
     public void print(){
-        System.out.println(id + "\t" + cost + "\t" + discount + "\t" + total);
+        System.out.println(id + "\t" + cost + " " + "\t" + discount + "\t" + "   " + (cost - discount));
     }
 }
-hi
